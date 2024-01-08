be903b028255d4da1cef5e22610c7fd1c4af4a43
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-08 14:33:08 +0000

    Chore/v2 rules (#1)
    * chore(eslintrc-ts): update supported envs
    
    update es2022 to es2023
    add jest
    add worker
    
    * chore(git): update .gitignore to exclude ide files
    
    * chore(eslint-ts): update rules for better code quality
    
    update max-lines-per-function to ignore blank lines
    add no-var
    add object-shorthand
    add prefer-template
    add no-return-await
    add require-await
    
    ---------
    
    Co-authored-by: gramsaroli <98903842+gramsaroli@users.noreply.github.com>

--- .eslintrc-ts.js
+++ .eslintrc-ts.js
@@ -16,7 +16,12 @@ module.exports = {
   rules: {
     'no-console': 'warn',
     'max-depth': ['error', { max: 1 }],
-    'max-lines-per-function': ['error', 17],
+    'max-lines-per-function': ['error', {'max': 17, 'skipBlankLines': true, 'skipComments': true}],
+    'no-var': 'error',
+    'object-shorthand': ['error', 'always', { avoidQuotes: true }],
+    'prefer-template': 'error',
+    'no-return-await': 'error',
+    'require-await': 'error',
     'max-params': ['error', 3],
     eqeqeq: 'error',
     'prefer-const': 'error',
@@ -25,7 +30,9 @@ module.exports = {
   },
   env: {
     browser: true,
-    es2021: true,
     node: true,
+    es2022: true,
+    jest: true,
+    worker: true
   },
 }
